a23147166f857b05ddd4ec484bd43cf664fc2538
Didgeridoo_Dave <malermeister-lehnartz@gmx.de>
2025-03-19 19:58:56 +0100

    added game screen, styling

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
   imports: [],
   templateUrl: './startscreen.component.html',
-  styleUrl: './startscreen.component.scss'
+  styleUrl: './startscreen.component.scss',
 })
 export class StartscreenComponent {
+  constructor(private router: Router) {}
 
+  startGame() {
+    this.router.navigateByUrl('/game');
+  }
 }
